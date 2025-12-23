3a23f527ffe8e5b631dddd5e53ba59c466ed1549
zitengjushi <106958158+zitengjushi@users.noreply.github.com>
2025-12-23 20:08:11 +0800

    Delete apk/fongmi/apk directory

--- apk/fongmi/apk/release.bat
+++
@@ -1,8 +0,0 @@
-move .\leanbackArm64_v8a\release\*.apk .\
-move .\leanbackArmeabi_v7a\release\*.apk .\
-move .\mobileArm64_v8a\release\*.apk .\
-move .\mobileArmeabi_v7a\release\*.apk .\
-rmdir /s /q .\leanbackArm64_v8a
-rmdir /s /q .\leanbackArmeabi_v7a
-rmdir /s /q .\mobileArm64_v8a
-rmdir /s /q .\mobileArmeabi_v7a
